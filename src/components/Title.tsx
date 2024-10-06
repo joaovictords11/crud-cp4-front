48c37de6572b3f102939ce9223fd7dbc337b5311
Joao Victor de Souza <joaovictordesouza9285@gmail.com>
2024-10-06 02:42:08 -0300

    melhorando estilo da pagina

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,10 @@
+import { FcTodoList } from "react-icons/fc";
+
 const PageTitle = () => {
   return (
-    <header className='flex justify-center p-6'>
-        <h1 className="text-5xl font-semibold text-gray-600">Todo List</h1>
+    <header className='flex justify-center items-center gap-4 p-6'>
+        <FcTodoList className="bg-gray-200 rounded-lg p-1" size={60}/>
+        <h1 className="text-5xl font-semibold text-gray-600">ToDo List</h1>
     </header>
   )
 }
