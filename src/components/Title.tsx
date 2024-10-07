ae45be2ce6ec9b5c7f0e2b72dcf4b43562622ea2
Joao Victor de Souza <joaovictordesouza9285@gmail.com>
2024-10-06 21:03:04 -0300

    ajuste responsividade

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,9 +2,9 @@ import { FcTodoList } from "react-icons/fc";
 
 const PageTitle = () => {
   return (
-    <header className='flex justify-center items-center gap-4 p-6'>
+    <header className='flex justify-center items-center gap-4 p-6 max-small:flex-wrap-reverse'>
         <FcTodoList className="bg-gray-200 rounded-lg p-1" size={60}/>
-        <h1 className="text-5xl font-semibold text-gray-600">ToDo List</h1>
+        <h1 className="text-5xl font-semibold text-gray-600 text-center">ToDo List</h1>
     </header>
   )
 }
